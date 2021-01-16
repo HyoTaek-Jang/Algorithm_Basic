6bdef4206fe7a3239c15d01eab0f139e15f4f568
HyoTaekJang <hyotaek9812@gmail.com>
2021-01-16 15:58:49 +0900

    implement 알고리즘 문제 풀이 추가

--- 2.Greedy/implementation.py
+++ 2.Greedy/implementation.py
@@ -4,3 +4,42 @@
 
 2차원 공간에서의 방향 벡터가 많이 활용됨
 '''
+
+
+def LRUD():
+    N = int(input())
+    plan = input().split(' ')
+    x, y = 1, 1
+    moveList = ['L', 'R', 'U', 'D']
+    enumList = enumerate(moveList)
+    movex = [-1, 1, 0, 0]
+    movey = [0, 0, -1, 1]
+
+    for move in plan:
+        for i in range(4):
+            if(move == moveList[i]):
+                dx = x+movex[i]
+                dy = y+movey[i]
+                if not(dx < 1 or dy < 1 or dx > N or dy > N):
+                    x, y = dx, dy
+    print(x,y)
+
+def night():
+    value = input()
+    col = ord(value[0])-ord('a')+1
+    row = int(value[1])
+    moveList = [(2,-1),(2,1),(-2,-1),(-2,1),(1,2),(-1,2),(1,-2),(-1,-2)]
+    count = 0
+    for move in moveList:
+        dcol = col + move[0]
+        drow = row + move[1]
+        if not (dcol < 1 or drow < 1 or dcol > 8 or drow > 8):
+            count += 1
+    print(count)
+
+
+# LRUD()
+# night()
+
+list = 'absc'.split()
+print(list)
