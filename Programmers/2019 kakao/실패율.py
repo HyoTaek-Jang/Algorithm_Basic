1117e0e4e6c8daf1c97234d07100d427ebd9ad73
HYOTAEK <hyotaek9812@gmail.com>
2021-03-18 20:50:53 +0900

    프로그래머스 2019 kakao 실패율

---
+++ Programmers/2019 kakao/실패율.py
@@ -0,0 +1,17 @@
+def solution(N, stages):
+    checkStage = [0 for i in range(N + 1)]
+
+    for i in stages:
+        for j in range(0, i):
+            checkStage[j] += 1
+
+            for k in range(0, N):
+        if checkStage[k + 1] == 0:
+            break
+        checkStage[k] = 1 - (checkStage[k + 1] / checkStage[k])
+
+    checkStage = list(enumerate(checkStage[:N]))
+    checkStage.sort(reverse=True, key=lambda x: x[1])
+
+    answer = [j[0] + 1 for j in checkStage]
+    return answer
